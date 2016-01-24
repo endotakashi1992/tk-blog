81a005962926ff2a91afd55a4d65742921cadc2e
遠藤孝志 <takashi@endoutakashi-no-MacBook-Air.local>
2016-01-24 23:38:40 +0900

    phase-4-compleated

--- src/Post.js
+++ src/Post.js
@@ -2,7 +2,17 @@ import React, { Component } from 'react';
 import Firebase from 'firebase'
 import ReactFireMixin from 'reactfire'
 import reactMixin from 'react-mixin';
+import {RaisedButton,AppBar,Card,CardMedia,CardHeader} from 'material-ui';
 
+let postStyles = {
+  card:{
+    margin:'20px',
+    padding:'30px'
+  },
+  text:{
+    float: 'left'
+  }
+}
 
 export default class Post extends Component {
   constructor(props) {
@@ -15,11 +25,15 @@ export default class Post extends Component {
   }
   render() {
     return (
-      <div>
+      <div style={{display:'flex'}}>
+        <Card style={postStyles.card}>
         <h1>{this.state.post.title}</h1>
         <p><i>{this.state.post.headline}</i></p>
-        <img src={this.state.post.image}></img>
-        <p>{this.state.post.body}</p>
+        <div >
+          <img style={{float:'left',margin:'30px',width:'50%'}} src={this.state.post.image}></img>
+          <p>{this.state.post.body}</p>
+        </div>
+        </Card>
       </div>
     );
   }
