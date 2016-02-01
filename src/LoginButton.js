7d0f13fd8970c0866d98a8000f855c4a0f1e70c9
遠藤孝志 <takashi@endoutakashi-no-MacBook-Air.local>
2016-02-02 02:23:09 +0900

    LoginWithCard

--- src/LoginButton.js
+++ src/LoginButton.js
@@ -10,40 +10,53 @@ let ref = new Firebase(`https://tks-blog.firebaseio.com/`);
 export default class LoginButton extends Component {
   constructor(props) {
     super(props);
-
+    this.state = {
+      isLogin : false
+    }
   }
   componentWillMount() {
     if(ref.getAuth()){
-      this.state = {isLogin:true};
+      console.log(ref.getAuth())
+      this.bindAsObject(ref.child("users").child(ref.getAuth().uid), "user");
+      ref.child("users").child(ref.getAuth().uid).on('value',()=>{
+        this.setState({isLogin:true})
+      })
     }else{
-      this.state = {isLogin:false};
+      // this.setState({isLogin:false})
+      // ref.onAuth((data)=>{
+      //   if(data){
+      //     this.setState({isLogin:true})
+      //     ref.child("users").child(data.uid).set(data);
+      //   }else{
+      //     this.setState({isLogin:false})
+      //   }
+      // })
     }
-    ref.onAuth((data)=>{
-      if(data){
-        this.setState({isLogin:true,user:{}})
-        ref.child("users").child(data.uid).set(data);
-      this.bindAsObject(ref.child("users").child(data.uid), "user");
-      }else{
-        this.setState({isLogin:false})
-      }
-    })
+
   }
   handleLogin() {
-    ref.authWithOAuthPopup("twitter", function(error, authData) {
+    ref.authWithOAuthPopup("twitter", function(error, data) {
       if (error) {
         console.log("Login Failed!", error);
       } else {
-        console.log("Authenticated successfully with payload:", authData);
+        console.log("Authenticated successfully with payload:", data);
+        ref.child("users").child(data.uid).set(data,()=>{
+          ref.child("users").child(data.uid).on('value',()=>{
+            this.setState({isLogin:true})
+          })
+          this.bindAsObject(ref.child("users").child(data.uid), "user");
+        });
       }
     });
   }
   handleLogout() {
     ref.unauth();
+    this.setState({isLogin:false})
   }
   render() {
     if(this.state.isLogin == false){
       return (
-        <RaisedButton label="LOGIN" onClick={this.handleLogin}>
+        <RaisedButton label="LOGIN" onClick={this.handleLogin.bind(this)}>
           <FontIcon className="muidocs-icon-custom-github" />
         </RaisedButton>
       );
@@ -51,7 +64,7 @@ export default class LoginButton extends Component {
       return (
           <div>
             <Avatar src={this.state.user.twitter.profileImageURL} />
-            <RaisedButton label="LOGOUT" onClick={this.handleLogout} />
+            <RaisedButton label="LOGOUT" onClick={this.handleLogout.bind(this)} />
           </div>
       );
     }
