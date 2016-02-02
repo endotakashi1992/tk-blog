a9eb71ff3160fea4ee3daecbfa2460036826fbed
遠藤孝志 <takashi@endoutakashi-no-MacBook-Air.local>
2016-02-02 22:32:06 +0900

    ok

--- src/Form.js
+++ src/Form.js
@@ -5,32 +5,49 @@ import reactMixin from 'react-mixin';
 import ReactFireMixin from 'reactfire'
 let Router = require('react-router')
 import {TextField,RaisedButton,Card,CardText,CardActions,FlatButton,Snackbar} from 'material-ui';
+import filepicker from 'filepicker-js'
+filepicker.setKey('AmtbglaVJTduac0cMyfDzz')
+
 
 let ref = new Firebase(`https://tks-blog.firebaseio.com/`);
+
+let styleContainer = {
+  image:{
+    width:'300px'
+  }
+}
 
 export default class Form extends Component {
   constructor(props) {
       super(props);
       this.state = {
         edit:false,
-        post:{}
+        imageUploaded:false,
+        image:"http://www.oradoko.jp/shop/images/noimage.jpg"
       };
+
   }
   componentDidMount() {
-    console.log(this.props.params.postId)
     if(this.props.params.postId){
       this.setState({edit:true})
       ref.child(`posts/${this.props.params.postId}`).on('value',(snap)=>{
         let _state = Object.assign(this.state,snap.val())
         _state.edit = true
-        console.log(_state)
         this.setState(_state)
       })
     }
   }
+  handleClickInput() {
+    self = this
+    filepicker.pick(
+  function(Blob){
+    self.setState({image:Blob.url})
+    self.setState({imageUploaded:true})
+  }
+);
+  }
   handleCreate() {
-    if(this.state.title && this.state.headline && this.state.body){
-      console.log("ok")
+    if(this.state.imageUploaded && this.state.title && this.state.headline && this.state.body){
     }else{
       this.setState({open:true})
       return;
@@ -49,7 +66,6 @@ export default class Form extends Component {
         name:ref.getAuth().twitter.username,
         image:ref.getAuth().twitter.profileImageURL
       }
-      this.state.image = 'http://www.oradoko.jp/shop/images/noimage.jpg'
       new_post.set(this.state,(e,data)=>{
         this.props.history.replace(`/posts/${postId}`);
       })
@@ -69,6 +85,9 @@ export default class Form extends Component {
             <TextField hintText="Title" valueLink={this.linkState('title')}/><br />
             <TextField hintText="headline" valueLink={this.linkState('headline')}/><br />
             <TextField multiLine="true" hintText="body" valueLink={this.linkState('body')}/><br />
+            <RaisedButton label="Image" secondary={true} onClick={this.handleClickInput.bind(this)}/>
+            <br />
+            <img src={this.state.image} style={styleContainer.image}/>
           </CardText>
           <CardActions>
             <FlatButton label="Save" onClick={this.handleCreate.bind(this)}/>
